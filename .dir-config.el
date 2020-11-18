e4fad9b0d9280f65de596e6fa154003230bba198
Pascal J. Bourguignon <pjb@informatimago.com>
2020-11-18 23:30:49 +0100

    Added .dir-config.el

---
+++ .dir-config.el
@@ -0,0 +1,10 @@
+((nil       . ((indent-tabs-mode . t)
+               (tab-width . 8)
+               (fill-column . 80)
+               (mode . auto-fill)))
+ (lisp-mode . ((indent-tabs-mode . t)
+               (tab-width . 8))))
+
+;; Local Variables:
+;; eval: (flycheck-mode -1)
+;; End:
