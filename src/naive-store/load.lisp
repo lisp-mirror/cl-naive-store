a31bed0c1d3bf96aad491568550d87fe3ed345a6
Pascal J. Bourguignon <pjb@informatimago.com>
2021-05-11 09:38:12 +0200

    Added to make-hash-table option :shared t for ccl.

--- src/naive-store/load.lisp
+++ src/naive-store/load.lisp
@@ -1,7 +1,7 @@
 (in-package :cl-naive-store)
 
 (defun find-collection-files (collection)
-  
+
   (let ((path (cl-fad:merge-pathnames-as-file (pathname (ensure-location collection))
 				   (make-pathname :directory '(:relative :wild-inferiors)
 						  :name :wild
@@ -14,7 +14,7 @@
 
     (unless files
       (naive-impl::debug-log (frmt "no files on path" ) :file-p t :args path))
-    
+
     files))
 
 ;;TODO: only pass shard and not filename ????
@@ -30,7 +30,7 @@
 ;;and have the user deal with it?
 (defmethod load-shard :around ((collection collection) shard filename &key &allow-other-keys)
   (let ((%loading-shard% shard))
-    
+
     (call-next-method)))
 
 ;;TODO: Add a catch all error thingy so that the status does not get stuck in :loading
@@ -40,19 +40,19 @@
 
 ;;TODO: add a wait with a time out if .lock file exists?
 (defmethod load-shard ((collection collection) shard filename &key &allow-other-keys)
-  
+
   (unless (equalp (status shard) :loading)
     (unless (probe-file (format nil "~a.lock" (location shard)))
       (let ((sexps))
 	;;(break "shit ~A" (status shard))
 	(setf (status shard) :loading)
-	
+
 	(naive-impl::debug-log (frmt "load-shard begin " ) :file-p t :args (list shard filename))
 	(with-open-file (in (or filename (location shard)) :if-does-not-exist :create)
 	  (when in
 
 	    ;;TODO: Reconsider this!!!
-	    
+
 	    ;;Reading the file and releasing it as soon as possible ... not sure it is a good
 	    ;;idea ... there is no difference in spead. It also doubles the memory use!!!
 	    (setf sexps
@@ -60,12 +60,12 @@
 			:while document-form
 			:collect document-form))
 	    (close in)
-	    
+
             (loop :for document-form in sexps
 		  :do (naive-impl::compose-document
 		       collection (or shard %loading-shard%) document-form)))))
 
-      
+
       (naive-impl::debug-log (frmt "load-shard end " ) :file-p t :args (list shard filename))
       (setf (status shard) :loaded))))
 
@@ -77,24 +77,30 @@
 
 ;;(cl-naive-task-pool:start-task-pool *task-pool*)
 
-(defparameter *files* (make-hash-table :synchronized t))
+(defparameter *files* (make-hash-table
+                       ;; LispWorks and Clozure CL default with thread safe hash-tables.
+			           ;; Other implementations will have to be checked.
+			           #+lispworks :single-thread #+lispworks nil
+			           #+(or sbcl ecl) :synchronized #+(or sbcl ecl) t
+			           ;; This hash table is used from several threads, therefore it must be :shared t in ccl.
+			           #+ccl :shared #+ccl t))
 
 
 
 (defmethod load-data ((collection collection) &key shard-macs (parallel-p t) &allow-other-keys)
-  
+
   (let ((tasks))
 
     (unless parallel-p
 
       (unless (> (length (shards collection)) 0)
 
-	
+
 	(let ((files (or (gethash collection *files*)
-			 (setf (gethash collection *files*) (find-collection-files collection))))) 
+			 (setf (gethash collection *files*) (find-collection-files collection)))))
 
 	  (naive-impl::debug-log (frmt "load-data" ) :file-p t :args files)
-	  
+
 	  (do-sequence (filename files :parallel-p nil)
 	    (multiple-value-bind (mac file)
 		(match-shard filename shard-macs)
@@ -102,22 +108,22 @@
 	      (unless mac
 		(setf mac (pathname-name filename))
 		(setf file filename))
-	      
+
 	      (when (or (not shard-macs)
 			file)
-		
+
 		(let ((shard (get-shard collection mac)))
 
 		  (unless shard
 		    (naive-impl::debug-log "loading load-shard"
 					     :file-p t
 					     :args (list shard filename))
-		    
+
 		    (setf shard (make-shard collection mac))
 		    (load-shard collection shard filename)
 		    (set-shard-cache-safe% collection mac shard)
 		    (vector-push-extend shard (shards collection))))))))))
-    
+
     (when parallel-p
       (unless (> (length (shards collection)) 0)
 	(let ((files (or (gethash collection *files*)
@@ -131,7 +137,7 @@
 		  (unless mac
 		    (setf mac (pathname-name filename))
 		    (setf file filename))
-		  
+
 		  (when (or (not shard-macs)
 			    file)
 
@@ -144,7 +150,7 @@
 					       :args (list shard filename))
 
 
-			
+
 			(push (;;cl-naive-task-pool:submit-task
 			       ;;*task-pool*
 			       lparallel:submit-task
@@ -199,14 +205,14 @@
 	  (when in
 	      (setf file-contents (read in nil))
 	      (close in)))
-	
+
 	(when file-contents
 	  (let ((collection
-		 (add-collection 
-		  store 
+		 (add-collection
+		  store
 		  (make-instance (collection-class store)
 				 :name (getx file-contents :name)
-				 :location (getx file-contents :location)			
+				 :location (getx file-contents :location)
 				 :filter (getx file-contents :filter)))))
 	    (when with-data-p
 	      (load-data collection))))))))
@@ -224,13 +230,13 @@
 	      (setf file-contents (read in nil))
 	      (close in)))
 	(when file-contents
-	  (let ((store (add-store 
+	  (let ((store (add-store
 			universe
 			(make-instance
 			 (store-class universe)
 			 :name (getx file-contents :name)
 			 :location (getx file-contents :location)))))
-	    
+
 	    (when (or with-collections-p with-data-p)
 	      (load-collections store with-data-p))))))))
 
@@ -240,5 +246,3 @@
 
 (defmethod load-store ((store store) &key with-data-p &allow-other-keys)
   (load-collections store with-data-p))
-
-
